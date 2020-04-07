7b6c0866071ab80259d5d08238164b1f49862ebb
dmrib <dmrib.cs@gmail.com>
2020-04-07 08:02:15 -0300

    Implement websocket connection

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { useState, useRef, useEffect } from 'react';
 import Content from './components/Content';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import config from './config/config.json';
 import './App.css';
 import 'antd/dist/antd.css';
+
+
+const CONNECTION_ADDRESS = `ws://${config['HOST']}:${config['PORT']}`
 
 
 /**
@@ -13,9 +18,49 @@ import 'antd/dist/antd.css';
  */
 function App()
 {
+    // component state hooks
+    const socket = useRef(null);
+    const [socketState, setSocketState] = useState(0);
+
+
+    /**
+     * I am called when this component is mounted.
+     */
+    useEffect(() =>
+    {
+        // create websocket connection
+        socket.current = new WebSocket(CONNECTION_ADDRESS);
+
+        // on connection opened event callback
+        socket.current.onopen = (event) =>
+        {
+            console.log('Websocket connection established.')
+            setSocketState(socket.current.readyState);
+        }
+
+        // on connection closed event callback
+        socket.current.onclose = (event) =>
+        {
+            console.log("Websocket connection closed.");
+            setSocketState(socket.current.readyState);
+        }
+
+        // on websocket error event callback
+        socket.current.onerror = (event) =>
+        {
+            console.log('Websocket error: ', {event});
+            setSocketState(socket.current.readyState);
+        }
+
+        // cleanup on lifecycle end
+        return () => socket.current.close();
+
+    }, []);
+
+
     return (
         <div className='app-container'>
-            <Header />
+            <Header socketState = {socketState}/>
             <Content />
             <Footer />
         </div>
