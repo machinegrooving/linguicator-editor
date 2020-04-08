929a9cd164aa182f453e4599ff2a5a06d0539cd1
dmrib <dmrib.cs@gmail.com>
2020-04-08 19:57:47 -0300

    Implement set maximum prediction size action

---
+++ src/store/actions/predictions.js
@@ -0,0 +1,20 @@
+/**
+ * I update predictions maximum size.
+ *
+ * @param {number} size - new predictions maximum size
+ *
+ * @returns {object} - action to be dispatched
+ */
+function setMaxSize(size) {
+    return {
+        type: 'SET_MAX_SIZE',
+        payload: {
+            size
+        }
+    };
+}
+
+
+export {
+    setMaxSize
+}
