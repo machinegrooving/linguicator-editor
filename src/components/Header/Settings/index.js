c02cd1fb34aed9e7603c27b5d8393420510b03bb
dmrib <dmrib.cs@gmail.com>
2020-04-09 10:44:42 -0300

    Make idle time until prediction adjustable

--- src/components/Header/Settings/index.js
+++ src/components/Header/Settings/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
-import { setMaxSize } from '../../../store/actions/predictions';
+import { setMaxSize, setIdleTime } from '../../../store/actions/predictions';
 import { SettingFilled } from '@ant-design/icons';
 import { Typography, Icon, Popover, Slider } from 'antd';
 import 'antd/dist/antd.css';
@@ -30,6 +30,19 @@ function Settings()
     }
 
 
+    /**
+     * I am a callback for idle time until prediction slider value changed.
+     *
+     * @param {number} time - new idle time until prediction
+     *
+     * @returns {undefined} - nothing
+     */
+    function setIdleSecondsUntilPrediction(time)
+    {
+        dispatch(setIdleTime(time * 1000));
+    }
+
+
     return(
         <Popover
             content={
@@ -52,6 +65,29 @@ function Settings()
                         }}
                         tooltipPlacement={"bottom"}
                         onAfterChange={setMaximumPredictionSize}
+                    >
+                    </Slider>
+                    <br/>
+                    <hr className='settings-separator'/>
+                    <br/>
+                    <center>
+                        <Typography.Text
+                            style={{
+                                color: '#a7425c'
+                            }}
+                        >
+                            Idle seconds until prediction:
+                        </Typography.Text>
+                    </center>
+                    <Slider
+                        min={3}
+                        max={15}
+                        defaultValue={3}
+                        style={{
+                            minWidth: 100,
+                        }}
+                        tooltipPlacement={"bottom"}
+                        onAfterChange={setIdleSecondsUntilPrediction}
                     >
                     </Slider>
             </div>
